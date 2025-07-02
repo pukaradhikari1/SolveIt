cd7dccb12ad3f930af947a433396aeb99950cbb1
SakaruKun <sakarmaharjan123@gmail.com>
2025-07-02 16:30:22 +0545

    started 😊
    bad start but we'll get there

--- Backend/app.py
+++ Backend/app.py
@@ -1,1 +1,11 @@
-# Lets start 
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home(name = "your name"):
+    return render_template("index.html",content = name, thislist = ["a", "b", "c", "d"])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
